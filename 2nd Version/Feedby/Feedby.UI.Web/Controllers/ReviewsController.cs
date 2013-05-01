0ce17e5444e6a2da89483bd7e5b4170fd1f6a2f1
Marcos Castany <marcos.castany@hotmail.com>
2013-05-01 20:18:29 -0300

    New compose page

---
+++ 2nd Version/Feedby/Feedby.UI.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Feedby.UI.Web.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        //
+        // GET: /Reviews/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Reviews/{userid}
+
+        public ActionResult Details(string userId)
+        {
+            return View();
+        }
+
+    }
+}
